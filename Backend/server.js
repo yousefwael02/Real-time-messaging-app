a5c4102a5303e8b9bd80de148547a9befd4f092d
Yousef Wael <yousef.wael2002@gmail.com>
2024-04-09 16:38:59 +0200

    Sign up endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -4,17 +4,19 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-const app = express();
-
 dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    // root route http://localhost:5000/
-    res.send("Hello World!");
-})
-
+app.use(express.json())
 app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//     // root route http://localhost:5000/
+//     res.send("Hello World!");
+// })
+
 
 app.listen(PORT, () => {
     connectToMongoDB();
