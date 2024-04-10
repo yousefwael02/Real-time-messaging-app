0dd4355fae5d00dc2e75527bd650a759de3f2aab
Yousef Wael <yousef.wael2002@gmail.com>
2024-04-10 02:26:47 +0200

    Logout Endpoint

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -78,5 +78,11 @@ export const signup = async (req, res) => {
 }
 
 export const logout = (req, res) => {
-    console.log("logoutUser");
+    try {
+        res.cookie("jwt", "", {maxAge: 0});
+        res.status(200).json({message: "Logged out successfully!"});
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
 }
