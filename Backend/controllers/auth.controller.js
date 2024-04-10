06e1907b3a1098949dacb1440ba899e3565195e4
Yousef Wael <yousef.wael2002@gmail.com>
2024-04-10 02:20:36 +0200

    Login endpoint

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -2,8 +2,28 @@ import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import generateTokenAndSetCookie from "../utils/generateToken.js";
 
-export const login = (req, res) => {
-    console.log("loginUser");
+export const login = async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne( { username } );
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if(!user || !isPasswordCorrect) {
+            return res.status(400).json({error: "Invalid username or password"});
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic
+        })
+    } catch (error)  {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
 }
 
 export const signup = async (req, res) => {
